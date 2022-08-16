218bbb324ffa309fd3d552b45d9d046a746016f5
Sean Pacheco <seanpacheco.code@gmail.com>
2022-08-16 15:11:44 -0400

    added mobile nav bar

--- public/main.js
+++ public/main.js
@@ -1,3 +1,6 @@
+
+//search bar autocomplete
+
 $(document).ready(function () {
     $('#title').autocomplete({
         source: async function(request,response) {
@@ -24,4 +27,22 @@ $(document).ready(function () {
     })
 })
 
+// toggle mobile menu
 
+const toggle = document.querySelector('.toggle')
+const menu = document.querySelector('.menu')
+ 
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active")
+        //adds menu icon
+        toggle.querySelector("a").innerHTML = "<i class ='fas fa-bars'></i>"
+    }else {
+        menu.classList.add("active")
+        //adds close icon
+        toggle.querySelector("a").innerHTML = "<i class ='fas fa-times'></i>"
+    }
+}
+
+//event listener
+toggle.addEventListener("click", toggleMenu, false)
