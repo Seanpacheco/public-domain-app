927569dac4ed851932d82e434f09dfd8a6d90178
Sean Pacheco <seanpacheco.code@gmail.com>
2022-09-22 22:48:49 -0400

    fixed addToFavorites page refresh

--- controllers/search.js
+++ controllers/search.js
@@ -25,21 +25,23 @@ module.exports = {
     },
     searchFilter: async (req,res) => {
         try{
-            rolesNum = req.body.roles
-            genreInp = req.body.genre
+            const rolesNum = req.body.roles
+            const genreInp = req.body.genre
             console.log(genreInp)
+            console.log(rolesNum)
+            console.log(req.user)
             if(rolesNum === ''){
                 const searchResult = await Play.find({genre: genreInp})
                 console.log (searchResult)
-                res.render('results.ejs',{plays: searchResult, user: req.user})
+                res.render('results.ejs',{plays: searchResult, user: req.user, genreInp: genreInp, rolesNum: rolesNum})
             }else if (genreInp === ''){
                 const searchResult = await Play.find({roles: rolesNum})
                 console.log (searchResult)
-                res.render('results.ejs',{plays: searchResult, user: req.user})
+                res.render('results.ejs',{plays: searchResult, user: req.user, genreInp: genreInp, rolesNum: rolesNum})
             }else{
                 const searchResult = await Play.find({roles: rolesNum, genre: genreInp})
                 console.log (searchResult)
-                res.render('results.ejs',{plays: searchResult, user: req.user})
+                res.render('results.ejs',{plays: searchResult, user: req.user, genreInp: genreInp, rolesNum: rolesNum})
             }
         }catch(err){
             console.log(err)
@@ -58,9 +60,11 @@ module.exports = {
     addToFavorites: async(req,res)=>{
         console.log(req.user)
         try{
-            await Favorite.create({title: req.body.title, playwright: req.body.playwright, userId: req.user.id, img: req.body.img, roles: req.body.roles, genre: (req.body.genre).split(','), year: req.body.year, synopsis: req.body.synopsis, characters: (req.body.characters).split(',')})
+            await Favorite.create({title: req.body.title, playwright: req.body.playwright, userId: req.user.id, img: req.body.img, roles: req.body.numOfRoles, genre: (req.body.genres).split(','), year: req.body.year, synopsis: req.body.synopsis, characters: (req.body.characters).split(',')})
+            req.flash('success', { msg: 'Added to Favorites' })
             // stops the browser from constantly loading.
-            res.status(204).send()
+            // res.status(204).send()
+            res.redirect(307,'back')
         } catch(error){
             console.error(error)
         }
